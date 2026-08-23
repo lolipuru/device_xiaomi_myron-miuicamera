e39cfafadfcaaec37ffd47201d5a6f34237ca596
lolipuru <banstellazz@gmail.com>
2026-08-23 09:00:42 +0000

    myron-miuicamera: update camera to OS4.0.0.15.XPMCNXM
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- shims/libgui_shim_miuicamera.c
+++ shims/libgui_shim_miuicamera.c
@@ -4,6 +4,20 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <stdint.h>
+#include <stdbool.h>
+
 void _ZN7android18BnProducerListener16onBufferDetachedEi() {
     return;
+}
+
+int _ZN7android19lockImageFromBufferENS_2spINS_13GraphicBufferEEEjRKNS_4RectEiPNS_11CpuConsumer12LockedBufferEb(
+        void* buffer, uint32_t usage, const void* rect, int fence, void* lockedBuffer, bool isFenceShared) {
+    (void)buffer;
+    (void)usage;
+    (void)rect;
+    (void)fence;
+    (void)lockedBuffer;
+    (void)isFenceShared;
+    return 0;
 }
